34b657604bbb3b2f3342208ba5440b4a5ff5fa26
Mikhail Tulkin <tulkin@robotct.ru>
2015-07-30 19:56:47 +0500

    no message

--- module/gamepad_control_module.h
+++ module/gamepad_control_module.h
@@ -1,55 +1,55 @@
 #ifndef Gamepad_CONTROL_MODULE_H
-#define	Gamepad_CONTROL_MODULE_H
-
+#define Gamepad_CONTROL_MODULE_H
 
 class GamepadControlModule : public ControlModule {
-	AxisData **Gamepad_axis;
+  AxisData **Gamepad_axis;
 
-	std::map<int, int> gamepad_axis_bind_to_module_axis;
-	std::map<system_value, AxisData*> axis;
+  std::map<int, int> gamepad_axis_bind_to_module_axis;
+  std::map<system_value, AxisData *> axis;
 
-	unsigned int COUNT_AXIS;
+  unsigned int COUNT_AXIS;
 
-	bool is_error_init;
+  bool is_error_init;
 
 #ifdef _WIN32
-	LPDIRECTINPUTDEVICE8W joystick;
-	DIJOYSTATE JState;
+  LPDIRECTINPUTDEVICE8W joystick;
+  DIJOYSTATE JState;
 #else
-	Joystick *joystick;
-	std::string InputDevice;
+  Joystick *joystick;
+  std::string InputDevice;
 #endif
 
-	colorPrintfModuleVA_t *colorPrintf_p;
+  colorPrintfModuleVA_t *colorPrintf_p;
 
-public:
-	GamepadControlModule() : is_error_init(false) {};
+ public:
+  GamepadControlModule() : is_error_init(false){};
 
-	//init
-	const char *getUID();
-	void prepare(colorPrintfModule_t *colorPrintf_p, colorPrintfModuleVA_t *colorPrintfVA_p);
+  // init
+  const char *getUID();
+  void prepare(colorPrintfModule_t *colorPrintf_p,
+               colorPrintfModuleVA_t *colorPrintfVA_p);
 
-	//compiler only
-	AxisData** getAxis(unsigned int *count_axis);
-	void *writePC(unsigned int *buffer_length);
+  // compiler only
+  AxisData **getAxis(unsigned int *count_axis);
+  void *writePC(unsigned int *buffer_length);
 
-	//intepreter - devices
-	int init();
-	void execute(sendAxisState_t sendAxisState);
-	void final() {};
+  // intepreter - devices
+  int init();
+  void execute(sendAxisState_t sendAxisState);
+  void final(){};
 
-	//intepreter - program & lib
-	void readPC(void *buffer, unsigned int buffer_length);
+  // intepreter - program & lib
+  void readPC(void *buffer, unsigned int buffer_length);
 
-	//intepreter - program
-	int startProgram(int uniq_index);
-	int endProgram(int uniq_index);
+  // intepreter - program
+  int startProgram(int uniq_index);
+  int endProgram(int uniq_index);
 
-	//destructor
-	void destroy();
-	~GamepadControlModule() {}
+  // destructor
+  void destroy();
+  ~GamepadControlModule() {}
 
-	void colorPrintf(ConsoleColor colors, const char *mask, ...);
+  void colorPrintf(ConsoleColor colors, const char *mask, ...);
 };
 
-#endif	/* Gamepad_CONTROL_MODULE_H */
+#endif /* Gamepad_CONTROL_MODULE_H */
